a0176096839555e9e182de7d7595186bd4d6bb5c
App Academy Student <student@appacademy.io>
2016-10-27 17:03:20 -0700

    remove debugging variables

--- skeleton/frontend/pokedex.jsx
+++ skeleton/frontend/pokedex.jsx
@@ -4,12 +4,8 @@ import configureStore from './store/store';
 import Root from './components/root';
 window._ = require('lodash');
 
-
-
-
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
-  window.store = store;
   const rootEl = document.getElementById('root');
   ReactDOM.render(<Root store={store} />, rootEl);
 });
